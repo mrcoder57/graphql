e4a8a1cbf7a71def65b6ab51bd69321fd0eb6efc
mrcoder57 <tiwariji2300@gmail.com>
2024-08-20 20:09:04 +0530

    logic to connect mongoDb atlas

---
+++ src/database/connect.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectToDb=async()=>{
+    try {
+        
+       await mongoose.connect(process.env.MONGODB_URI!);
+       console.log("connected to db")
+        const connect=mongoose.connection
+        connect.on('connected',()=>{
+            console.log("connected to db")
+        })
+    } catch (error) {
+        console.log("an error occured while connecting to db",error)
+        process.exit()
+    }
+}
+export default connectToDb
